58b3d46c924b3a0aad11ce7b360755b2a23ab907
stefanozuffi <stefano.zuffi06@outlook.com>
2025-12-02 11:13:53 +0100

    creation of a password by concatenating input data + get sum of birthDate

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,7 +1,35 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
     public static void main() {
-        System.out.println("Init!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to your PassWord Generator\nPlease provide the required information");
+        
+        System.out.println("What is your first name?");
+        String firstName = input.nextLine();
+
+        System.out.println("What is your last name?");
+        String lastName = input.nextLine();
+
+        System.out.println("What is your favourite color?");
+        String favColor = input.nextLine();
+
+        System.out.println("When where you born (please indicate in the format: dd/mm/yyyy)?");
+        String dateBirth = input.nextLine();
+        String[] dateBirthTerms = dateBirth.split("/");
+        int dateSum = 0;
+        for (int i=0; i < dateBirthTerms.length; i++) {
+            dateSum += Integer.parseInt(dateBirthTerms[i]);
+        }
+
+
+        input.close();
+
+        System.out.println(firstName  + '-' + lastName + '-' + favColor + '-' + dateSum);
+
     }
 }
